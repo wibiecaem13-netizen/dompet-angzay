c928b4dc3caf35e8a9cba9c7a4f3dd0cd2681859
wibiecaem13-netizen <wibiecaem13@gmail.com>
2026-07-26 14:06:10 +0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    "./index.html",
+    "./app.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
